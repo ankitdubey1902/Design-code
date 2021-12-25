9d385e51b00a042fd70cfea119e0d7da3309c0a8
ankitdubey1902 <61725942+ankitdubey1902@users.noreply.github.com>
2021-12-25 13:24:18 +0530

    Second commit
    Second commit

--- main.js
+++ main.js
@@ -2,8 +2,13 @@ var prevScrollpos = window.pageYOffset;
       window.onscroll = function () {    
         if(window.pageYOffset > 20){
             document.getElementById("container").classList.add("box-shad");
+            document.getElementById("logo").innerHTML = "DC";            
         }
         else{
             document.getElementById("container").classList.remove("box-shad");
+            document.getElementById("logo").innerHTML = "Design Code";            
         }
       };
+
+      var scene = document.getElementById('img');
+      var parallax = new Parallax(scene);      
